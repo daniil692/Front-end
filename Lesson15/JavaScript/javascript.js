c1c068704371144e86578edff2e85350c6ddaf76
Daniil Barmanov <barmanov04@gmail.com>
2020-11-13 18:58:50 +0200

    Lesson15 was modified

--- Lesson15/JavaScript/javascript.js
+++ Lesson15/JavaScript/javascript.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+/*console.log('Hello');
+var a
+let b
+const c*/
+
+/*let myVariable;
+myVariable = 4;
+const myConst = 5;
+console.log(my);*/
+
+let seconds = 3665;
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = 3600;
+let hours = seconds / SECONDS_IN_HOUR
+console.log('Hours: ' + hours);
