01c1abae5453befbe6ff9ee7bce008dfd7063788
Daniil Barmanov <barmanov04@gmail.com>
2020-11-20 18:55:50 +0200

    Lessons 16,17 were added

---
+++ Lesson17/JavaScript/js.js
@@ -0,0 +1,60 @@
+// let result = prompt("Введите количество лет ")
+// let age = "Автору ";
+
+// if (result == null){
+//     alert('Вы не ввели число')
+// }
+// else{
+//     if (result >= 5 && result <= 20) {
+//         age += result;
+//         age += " лет";
+//     } 
+//     else if ((result % 10) == 1) {
+//         age += result;
+//         age += " год";
+//     } 
+//     else if ((result % 10) >= 2 && (result % 10) <= 4) {
+//         age += result;
+//         age += " годa";
+//     } 
+//     else {
+//         age += result;
+//         age += " лет";
+//     }
+//     alert(age);    
+// }
+
+
+// let age = prompt('Сколько тебе лет?')
+// let status = age >= 18 ? "Совершеннолетний" : "Несовершеннолетний";
+// if(age >= 18){
+//     alert('Совершеннолетний')
+// }
+// else(age <= 18); {
+//     alert('Несовершеннолетний')
+// }
+// alert(status)
+
+
+let season = prompt('Какой сейчас сезон?');
+
+switch(season){
+
+    case "leto":
+        alert('zharko');
+        break
+
+    case "zima":
+        alert('holodno')
+        break
+    
+    case "osen":
+        alert('namana')
+        break
+    
+    case "vesna":
+        alert('namana')
+        break
+    
+}
+
